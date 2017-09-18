69f5eb731c285c5db8989436080ffab81db799ee
OrestiArsi <orestiarsi@gmail.com>
2017-09-18 15:05:16 -0400

    added confirmable for devise and rubocop.yml --need to revisit rubocop--

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
+#frozen_string_literal: true
+
+#user model for our app 
+
 class User < ApplicationRecord
 
 
@@ -6,7 +10,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
 
 has_many :accounts, foreign_key: :owner_id
 end
